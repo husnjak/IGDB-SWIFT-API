1b2c8cb44b4f978f18edeb8dcd613adbe8acc3d1
husnjak <filip@igdb.com>
2023-05-23 08:48:16 +0200

    Updated Protofile (#19)
    * Updated Protofile for ReleaseDateStatus

--- IGDB-SWIFT-API.podspec
+++ IGDB-SWIFT-API.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name                  = 'IGDB-SWIFT-API'
-  s.version               = '0.4.1'
+  s.version               = '0.4.2'
   s.summary               = 'Wrapper for IGDBs API written in Swift.'
   s.description           = "A Swift wrapper for the IGDB.com Free Video Game Database API."
   s.documentation_url     = "https://api-docs.igdb.com"
@@ -13,6 +13,6 @@ Pod::Spec.new do |s|
   s.swift_version         = '5.0'
   s.source_files          = 'Sources/IGDB-SWIFT-API/**/*.swift'
 
-  s.dependency 'SwiftProtobuf', '~> 1.20.3'
+  s.dependency 'SwiftProtobuf', '~> 1.21.0'
   s.dependency 'Just', '~> 0.8.0'
 end
--- Sources/IGDB-SWIFT-API/Wrapper/IGDBWrapper.swift
+++ Sources/IGDB-SWIFT-API/Wrapper/IGDBWrapper.swift
@@ -9,22 +9,24 @@
 import Foundation
 import Just
 
-private var APIURL = "https://api.igdb.com/v4"
+private let APIURL = "https://api.igdb.com/v4"
 
 public class IGDBWrapper {
     private var requestHeaders = ["x-user-agent": "igdb-api-swift"]
+    private var requestURL = APIURL
+    
     public init(clientID: String, accessToken: String) {
         requestHeaders = ["x-user-agent": "igdb-api-swift", "client-id": clientID, "authorization": "Bearer \(accessToken)"]
     }
     
     public init(proxyURL: String, proxyHeaders: [String: String]) {
-        APIURL = proxyURL
+        requestURL = proxyURL
         requestHeaders = proxyHeaders
         requestHeaders["x-user-agent"] = "igdb-api-swift"
     }
     
     public func apiProtoRequest(endpoint: Endpoint, apicalypseQuery: String, dataResponse: @escaping (Data) -> (Void), errorResponse: @escaping (RequestException) -> (Void)) {
-        let requestURL = "\(APIURL)\(endpoint.url()).pb"
+        let requestURL = "\(requestURL)\(endpoint.url()).pb"
         Just.post(requestURL, headers: requestHeaders, requestBody: apicalypseQuery.data(using: .utf8, allowLossyConversion: false), asyncCompletionHandler:  { response in
             if response.statusCode != 200 {
                 errorResponse(RequestException(statusCode: response.statusCode ?? -1, url: requestURL, msg: response.text ?? ""))
@@ -34,7 +36,7 @@ public class IGDBWrapper {
     }
     
     public func apiJsonRequest(endpoint: Endpoint, apicalypseQuery: String, dataResponse: @escaping (String) -> (Void), errorResponse: @escaping (RequestException) -> (Void)) {
-        let requestURL = "\(APIURL)\(endpoint.url())"
+        let requestURL = "\(requestURL)\(endpoint.url())"
 
         Just.post(requestURL, headers: requestHeaders, requestBody: apicalypseQuery.data(using: .utf8, allowLossyConversion: false), asyncCompletionHandler:  { response in
             if response.statusCode != 200 {
@@ -46,7 +48,7 @@ public class IGDBWrapper {
     
 //    Adding count functions
     public func apiProtoCountRequest(endpoint: Endpoint, apicalypseQuery: String, dataResponse: @escaping (Data) -> (Void), errorResponse: @escaping (RequestException) -> (Void)) {
-        let requestURL = "\(APIURL)\(endpoint.url())/count.pb"
+        let requestURL = "\(requestURL)\(endpoint.url())/count.pb"
         Just.post(requestURL, headers: requestHeaders, requestBody: apicalypseQuery.data(using: .utf8, allowLossyConversion: false), asyncCompletionHandler:  { response in
             if response.statusCode != 200 {
                 errorResponse(RequestException(statusCode: response.statusCode ?? -1, url: requestURL, msg: response.text ?? ""))
@@ -56,7 +58,7 @@ public class IGDBWrapper {
     }
     
     public func apiJsonCountRequest(endpoint: Endpoint, apicalypseQuery: String, dataResponse: @escaping (String) -> (Void), errorResponse: @escaping (RequestException) -> (Void)) {
-        let requestURL = "\(APIURL)\(endpoint.url())/count"
+        let requestURL = "\(requestURL)\(endpoint.url())/count"
 
         Just.post(requestURL, headers: requestHeaders, requestBody: apicalypseQuery.data(using: .utf8, allowLossyConversion: false), asyncCompletionHandler:  { response in
             if response.statusCode != 200 {
